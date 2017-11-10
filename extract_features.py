52fc4825bb59a4ab4d78f0779014d439f5f8b80f
ggiancarlos <ggutierrezcervantes@gmail.com>
2017-11-10 16:36:35 -0500

    new method added for the feature extraction of the patches

--- extract_features.py
+++ extract_features.py
@@ -1,7 +1,7 @@
 import os
 import pickle
 
-from skimage import io, util
+from skimage import io, util, color
 from Tools import config as cfg, feature_extractor
 
 inputPath = cfg.inputFoldePath
@@ -34,6 +34,25 @@ def extractAndStoreFeatures(inputFolder, outputFolder):
         outputFile = open(outputPath, 'wb')
         pickle.dump(feats, outputFile)
         outputFile.close()
+
+
+def extractAndStoreFeaturesForPatches(imagesList):
+    print('Extracting features...')
+    matrixOfFeatsPerImage = list()
+
+    for img in imagesList:
+        print('Processing Image')
+        featsList = list()
+        for patch in img:
+            print('Extracting features of patch ... from image ...')
+            #image = io.imread(patch, as_grey=True)
+            image = color.rgb2grey(patch)
+            image = util.img_as_uint(image)
+            feats = feature_extractor.extractHOGFeatures(image)
+            featsList.append(feats)
+        matrixOfFeatsPerImage.append(featsList)
+
+    return matrixOfFeatsPerImage
 
 
 if __name__ == '__main__':
